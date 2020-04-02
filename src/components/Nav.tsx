25bc84d0e7482aa836268bfb4c4308422a0b0cc0
Brandon Dong <gasingdong@gmail.com>
2020-04-02 11:42:14 -0700

    WIP styles

---
+++ src/components/Nav.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Nav: React.FC = () => {
+  return (
+    <nav className="nav">
+      <ul>
+        <li>PROJECTS</li>
+        <li>RESUME</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
