9e566a8e90437ce0669e622dc0b13af35bcd1d77
Brandon Dong <gasingdong@gmail.com>
2020-04-17 12:42:02 -0700

    Updates

---
+++ precache-manifest.bbf361b122de89727e1061cfa00c465e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fe80e3256345407ce89ed0a77000bef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1f53ad4258fa7dfdd5f",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "70d3e45c615c0fe47825",
+    "url": "/static/css/main.057938ae.chunk.css"
+  },
+  {
+    "revision": "a1f53ad4258fa7dfdd5f",
+    "url": "/static/js/2.1644ca07.chunk.js"
+  },
+  {
+    "revision": "70d3e45c615c0fe47825",
+    "url": "/static/js/main.e1a2abe8.chunk.js"
+  },
+  {
+    "revision": "7d2dccf2ab0fcc6c934a",
+    "url": "/static/js/runtime-main.c83fe2f4.js"
+  },
+  {
+    "revision": "80be508a6269e30c9c242e2738857a57",
+    "url": "/static/media/avatar.80be508a.jpg"
+  }
+]);
