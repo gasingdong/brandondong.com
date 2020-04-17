79d2077e7bbdf1ab08dda4b6d29d07ce1117dfca
Brandon Dong <gasingdong@gmail.com>
2020-04-17 12:17:37 -0700

    add 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Content from './components/Content';
+import NotFound from './components/NotFound';
 
 const App: React.FC = () => {
   return (
     <Router>
       <div>
         <Header />
-        <Content />
+        <Switch>
+          <Route exact path="/">
+            <Content />
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
       </div>
     </Router>
   );
